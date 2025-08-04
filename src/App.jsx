ea97b248a4fca242a9a45d02779d8213d5bef7ca
leos-lab <lab@leoleung.co.uk>
2025-08-04 02:47:04 +0100

    fix main and app jsx for blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import EventGallery from './pages/EventGallery'
 import AdminPanel from './pages/admin/AdminPanel'
 
 export default function App({ apiBase }) {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home apiBase={apiBase} />} />
-        <Route path="/event/:slug" element={<EventGallery apiBase={apiBase} />} />
-        <Route path="/event/:slug/admin" element={<AdminPanel apiBase={apiBase} />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home apiBase={apiBase} />} />
+      <Route path="/event/:slug" element={<EventGallery apiBase={apiBase} />} />
+      <Route path="/event/:slug/admin" element={<AdminPanel apiBase={apiBase} />} />
+    </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
 
-const apiBase = import.meta.env.VITE_API_BASE || ''
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App apiBase={apiBase} />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 )
