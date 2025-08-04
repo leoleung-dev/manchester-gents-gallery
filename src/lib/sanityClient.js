ed423b126cc49419a4f2c921ad76d923147a2eac
leos-lab <lab@leoleung.co.uk>
2025-08-04 02:22:03 +0100

    Update sanityClient.js

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -1,11 +1,16 @@
-// src/lib/sanityClientClient.js
 import { createClient } from '@sanity/client'
+import imageUrlBuilder from '@sanity/image-url'
 
 const client = createClient({
   projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
   dataset: import.meta.env.VITE_SANITY_DATASET,
   apiVersion: '2023-08-03',
-  useCdn: true, // faster, safe for read-only
+  useCdn: false,
 })
+
+const builder = imageUrlBuilder(client)
+export function urlFor(source) {
+  return builder.image(source)
+}
 
 export default client
