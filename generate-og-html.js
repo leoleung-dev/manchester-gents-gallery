cfa83aeb723d5730ac503f0542c344629eea09d5
leos-lab <lab@leoleung.co.uk>
2025-08-05 16:00:17 +0100

    Update generate-og-html.js

--- generate-og-html.js
+++ generate-og-html.js
@@ -25,20 +25,27 @@ async function run() {
     const filePath = path.join(dirPath, 'index.html');
 
     const html = `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <title>Manchester Gents | ${event.title}</title>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Manchester Gents | ${event.title}</title>
 
-    <meta property="og:title" content="Manchester Gents | Event Gallery " />
-    <meta property="og:description" content="View photos from: ${event.title}" />
-    <meta property="og:image" content="https://mg-og-generator.vercel.app/api/og?slug=${event.slug}" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://photos.manchestergents.com/event/${event.slug}" />
-    <meta name="twitter:card" content="summary_large_image" />
-  </head>
-  <body></body>
-</html>`;
+      <!-- Open Graph Meta Tags -->
+      <meta property="og:title" content="Manchester Gents | View photos from: ${event.title}" />
+      <meta property="og:description" content="See all the best shots from ${event.title}!" />
+      <meta property="og:image" content="https://mg-og-generator.vercel.app/api/og?slug=${event.slug}" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://photos.manchestergents.com/event/${event.slug}" />
+
+      <!-- Twitter Card Meta Tags -->
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Manchester Gents | View photos from: ${event.title}" />
+      <meta name="twitter:description" content="See all the best shots from ${event.title}!" />
+      <meta name="twitter:image" content="https://mg-og-generator.vercel.app/api/og?slug=${event.slug}" />
+    </head>
+    <body></body>
+  </html>`;
 
     fs.mkdirSync(dirPath, { recursive: true });
     fs.writeFileSync(filePath, html, 'utf-8');
